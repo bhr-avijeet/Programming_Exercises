eb830e967bed8c9a75d0d53c53144b578c1e2fdd
bhr-avijeet <32174324+bhr-avijeet@users.noreply.github.com>
2023-10-20 19:14:17 +0530

    Enhanced the code.

--- Python Problems/RemoveDuplicates/RemoveDuplicates.py
+++ Python Problems/RemoveDuplicates/RemoveDuplicates.py
@@ -2,15 +2,7 @@ def remove_duplicates(nums):
     k = 0
     dup = []
     for i in range(len(nums)):
-        if nums[i] in dup:
-            nums.remove(nums[i])
-            i += 1
-        else:
-            k += 1
+        if nums[i] not in dup:
             dup.append(nums[i])
-        print(nums)
-    print(k)
-
-
-
+    print(dup)
 remove_duplicates([0,0,1,1,1,2,2,3,3,4])
